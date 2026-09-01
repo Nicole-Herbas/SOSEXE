92b7fa23433d7cde8097c06f8102d98b5837fd1c
Nicole <nicole.herbas@ucb.edu.bo>
2026-08-31 21:19:28 -0400

    First commit

---
+++ sos-backend/src/test/java/bo/edu/sos/backend/SosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.sos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
