5e8a0ce765ba9ce16d770ab09032ac6e27de0b0d
pochis <jose.camacho.s1@ucb.edu.bo>
2026-09-15 16:14:42 -0400

    fix: se agregaron variables reutilizable spara strings de html y npm test de mapa

--- sos-frontend/src/app/features/mapa/pages/mapa/mapa.ts
+++ sos-frontend/src/app/features/mapa/pages/mapa/mapa.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { MapaLeaflet } from '../../components/mapa-leaflet/mapa-leaflet';
+import {
+  DONACIONES_ACEPTADAS,
+  FILTROS_NECESIDAD_MAPA,
+  FILTROS_TIPO_MAPA,
+  MAPA_TEXTOS,
+  NECESIDADES_PRIORITARIAS,
+  PUNTOS_MAPA,
+} from '../../constants/mapa.constants';
 
 @Component({
   selector: 'app-mapa',
@@ -10,5 +18,22 @@ import { MapaLeaflet } from '../../components/mapa-leaflet/mapa-leaflet';
   styleUrl: './mapa.scss'
 })
 export class Mapa {
+  readonly textos = MAPA_TEXTOS;
+  readonly filtrosTipo = FILTROS_TIPO_MAPA;
+  readonly filtrosNecesidad = FILTROS_NECESIDAD_MAPA;
+  readonly puntos = PUNTOS_MAPA;
+  readonly necesidadesPrioritarias = NECESIDADES_PRIORITARIAS;
+  readonly donacionesAceptadas = DONACIONES_ACEPTADAS;
+
+  filtroTipoActivo: string = FILTROS_TIPO_MAPA[0].id;
+  filtroNecesidadActivo: string = FILTROS_NECESIDAD_MAPA[0].id;
+
+  seleccionarFiltroTipo(id: string): void {
+    this.filtroTipoActivo = id;
+  }
+
+  seleccionarFiltroNecesidad(id: string): void {
+    this.filtroNecesidadActivo = id;
+  }
 
 }
